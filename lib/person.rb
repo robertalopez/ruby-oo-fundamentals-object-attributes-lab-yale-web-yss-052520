521962fff1077a0cba0cd46e35de0386724fedfb
robertalopez <roblop1234@yahoo.com>
2020-05-25 17:07:59 -0700

    done

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person 
+    # def initialize(name, job)
+    #     @name = name 
+    #     @job = job 
+    # end 
+
+    def name
+        @name
+    end 
+
+    def name=(newname)
+        @name = newname
+    end 
+
+    def job 
+        @job 
+    end 
+
+    def job=(newjob)
+        @job = newjob
+    end 
+end 
